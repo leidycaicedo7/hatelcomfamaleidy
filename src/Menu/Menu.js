9725aa1981d9110845c05c4d4189fb20e0b13f4f
308 <308@academicos.cesde.edu.co>
2022-09-15 20:38:11 -0500

    se hace el render de las habitaciones

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -14,6 +14,7 @@ export function Menu() {
             <li className="nav-item">
               <Link className="nav-link" to="habitaciones">Habitaciones</Link>
             </li>
+            
            </ul>
           <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
